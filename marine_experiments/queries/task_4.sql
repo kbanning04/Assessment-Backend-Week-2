e35656815473238dd59b2b2a74ec9fbdd3598fcd
kbanning04 <kbanning04@gmail.com>
2026-05-15 15:33:53 +0100

    Queries Task 4

--- marine_experiments/queries/task_4.sql
+++ marine_experiments/queries/task_4.sql
@@ -1,11 +1,18 @@
 SELECT
     sp.species_name,
     e.experiment_id,
-    sp.is_predator,
 CASE
-    WHEN sp.is_predator = 't' THEN e.score * 1.2
+    WHEN sp.is_predator = 't'
+    THEN 'True'
 ELSE
-    e.score * 1
+    'False'
+END AS is_predator,
+CASE
+    WHEN is_predator = 'True' 
+    THEN 
+        e.score * 1.2 
+ELSE
+    e.score * 1 
 END AS score
 FROM
     subject AS sb
@@ -16,4 +23,4 @@ JOIN
     experiment AS e
     USING (subject_id)
 ORDER BY
-    e.score DESC;
+    score DESC;
