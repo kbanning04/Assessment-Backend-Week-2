c8e8450e9381771390fb75032395d2f3435caa92
kbanning04 <kbanning04@gmail.com>
2026-05-15 14:21:54 +0100

    Queries Task 1 - voltuneered?

--- marine_experiments/queries/task_1.sql
+++ marine_experiments/queries/task_1.sql
@@ -1,1 +1,12 @@
+SELECT
+    sb.subject_id,
+    sb.subject_name,
+    sp.species_name,
+    to_char(sb.date_of_birth, 'YYYY-MM') AS date_of_birth
+FROM 
+    subject AS sb
+JOIN
+    species AS sp
+    ON (sb.species_id = sp.species_id)
+ORDER BY date_of_birth DESC;
 
